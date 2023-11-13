325b77c57f232538db9828fba0e1e9b3dbc83726
tnyandoro <tnyandoro@gmail.com>
2023-11-13 11:17:57 +0200

    Push initial commt

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+  get 'about', to: 'pages#about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+end
